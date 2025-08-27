a9e2d872edce15fb3a2c6398645bb2f7702eaebe
MaitriThummar2912 <maitrithummar29@gmail.com>
2025-08-27 19:33:00 +0530

    Add files via upload

---
+++ Maitri28.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter the value of a:");
+    scanf("%d",&a);
+    if(a/7==0)
+    {
+        printf("a is devided by 7",a);
+    }
+    else
+    {
+        printf("a is not devided by 7",a);
+    }
+}
