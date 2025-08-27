a843ef6ed496138248df7b65bbd6885d46629147
MaitriThummar2912 <maitrithummar29@gmail.com>
2025-08-27 19:31:59 +0530

    Add files via upload

---
+++ Maitri25.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    float a,b;
+    printf("Enter two values:");
+    scanf("%f%f",&a,&b);
+    if(a>b)
+    printf("%f>%f",a,b);
+    else
+    if(a<b)
+    printf("%f<%f",a,b);
+    else
+    printf("%f=%f",a,b);
+
+}
