5c14268d929d5cf045fcfab05241b6e892a1828b
MaitriThummar2912 <maitrithummar29@gmail.com>
2025-08-27 19:30:43 +0530

    Add files via upload

---
+++ Maitri21.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    float net_salary,gross_salary,allowance,deduction;
+    printf("Enter gross salary:");
+    scanf("%f",&gross_salary);
+    allowance=0.1*gross_salary;
+    deduction=0.03*gross_salary;
+    net_salary=gross_salary + allowance - deduction;
+    printf("allowance=%f\n",allowance);
+    printf("deduction=%f\n",deduction);
+    printf("net_salary=%f\n",net_salary);
+    
+}
