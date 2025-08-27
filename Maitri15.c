88590ffdc8f15fc99bf26bda2bde3e48446f2272
MaitriThummar2912 <maitrithummar29@gmail.com>
2025-08-27 19:27:42 +0530

    Add files via upload

---
+++ Maitri15.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    float F,C;
+    printf("Enter tha value of Fahrenheit:");
+    scanf("%f",&F);
+    C=5/9 * (F-32);
+    printf("The value of Celcius is %f",C);
+}
