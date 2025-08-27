5046ca94c193a9e2a6ab428150179086c5435eed
MaitriThummar2912 <maitrithummar29@gmail.com>
2025-08-27 19:29:37 +0530

    Add files via upload

---
+++ Maitri19.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    float radius,Area;
+    printf("Enter the value of radius:");
+    scanf("%f",&radius);
+    Area= 22/7 * radius *radius;
+    printf("The Area of a ciecle is %f",Area);
+}
