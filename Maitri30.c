1c94c038ce69f07c785fe365f192744e51906b7b
MaitriThummar2912 <maitrithummar29@gmail.com>
2025-08-27 19:33:41 +0530

    Add files via upload

---
+++ Maitri30.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    float maths_marks,chemistry_marks,physics_marks,total,average;
+    printf("Enter the marks:");
+    scanf("%f%f%f",&maths_marks,&chemistry_marks,&physics_marks);
+    if(average >= )
+}
